56dd228cb0b99e2b9cf6c4bb228ae2ee6a39bf32
Mark Pokorny <markpokornycos@gmail.com>
2019-03-08 12:30:11 -0700

    installs 3.0.0-beta.3

--- hello-tvos/ViewController.swift
+++ hello-tvos/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import LaunchDarkly_tvOS
 
 class ViewController: UIViewController {
 
--- hello-tvos/hello-tvos-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  hello-tvos-Bridging-Header.h
-//  hello-tvos
-//
-//  Created by Danial Zahid on 4/9/17.
-//  Copyright © 2017 LaunchDarkly. All rights reserved.
-//
-
-@import LaunchDarkly;
